0b3607ff6d31d4010015d209dbb3f77ed1b615b0
CalumMcLean29 <140830972+CalumMcLean29@users.noreply.github.com>
2023-08-04 23:49:49 +0100

    Update al_prc_upsert_bank.sql
    Add unique bank_id - will need this for linking balance/transactions

--- al_prc_upsert_bank.sql
+++ al_prc_upsert_bank.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE PROCEDURE al_prc_upsert_bank(p_bank_name VARCHAR2,
+create or replace NONEDITIONABLE PROCEDURE al_prc_upsert_bank(p_bank_name VARCHAR2,
                                                p_account_number VARCHAR2,
                                                p_sort_code VARCHAR2,
                                                p_account_moniker VARCHAR2,
@@ -13,9 +13,9 @@ BEGIN
                        b.bank_name = p_bank_name)
         WHEN NOT MATCHED THEN 
             INSERT 
-            (bank_name, account_number,sort_code, account_moniker, account_type)
+            (bank_name, account_number,sort_code, account_moniker, account_type, bank_id)
             VALUES
-            (p_bank_name, p_account_number, p_sort_code, p_account_moniker, p_account_type)
+            (p_bank_name, p_account_number, p_sort_code, p_account_moniker, p_account_type, al_seq_bank_id.nextval)
         WHEN MATCHED THEN
             UPDATE SET account_moniker = p_account_moniker
             WHERE account_moniker != p_account_moniker;
